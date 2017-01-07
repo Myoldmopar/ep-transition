3fa4c9759ff58229839c10fae017ca28d0e1e061
Edwin Lee <leeed2001@gmail.com>
2017-01-07 12:42:31 -0700

    Continued adding test stubs; now one test file per lib file

---
+++ test/idf/test_version.py
@@ -0,0 +1,9 @@
+import unittest
+from transition.version import START_VERSION, END_VERSION, VersionInformation
+
+
+class TestVersions(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertTrue(isinstance(START_VERSION, VersionInformation))
+        self.assertTrue(isinstance(END_VERSION, VersionInformation))
---
+++ test/test_manager.py
@@ -0,0 +1,9 @@
+import unittest
+from transition.manager import TransitionFile
+
+
+class TestManager(unittest.TestCase):
+
+    def test_dummy(self):
+        TransitionFile(None)
+        self.assertTrue(True)
