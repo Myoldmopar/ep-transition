d419ec43f313be3721e03a5c9f427ca0ff684dac
Edwin Lee <leeed2001@gmail.com>
2017-01-07 19:48:46 -0700

    Improve coverage

--- test/test_driver.py
+++ test/test_driver.py
@@ -1,21 +1,18 @@
-import os
-import subprocess
 import unittest
+
+import driver
 
 
 class TestDriver(unittest.TestCase):
     def test_driver(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        os.chdir(dir_path)
-        driver_path = os.path.join(dir_path, "..", "driver.py")
         # usage mode
-        r = subprocess.call([driver_path, "usage"], stdout=open(os.devnull, 'wb'))
+        r = driver.drive(['program_name', 'usage'])
         self.assertEqual(0, r)
         # incorrect number of args
-        r = subprocess.call([driver_path], stdout=open(os.devnull, 'wb'))
+        r = driver.drive(['program_name'])
         self.assertEqual(1, r)
-        r = subprocess.call([driver_path, "hi", "what"], stdout=open(os.devnull, 'wb'))
+        r = driver.drive(['program_name', 'usage', 'what'])
         self.assertEqual(1, r)
         # bad arg mode
-        r = subprocess.call([driver_path, "badarg"], stdout=open(os.devnull, 'wb'))
+        r = driver.drive(['program_name', 'badarg'])
         self.assertEqual(1, r)
