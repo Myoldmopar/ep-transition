5de65b64625d6330515285aa618dd2312341b4d4
Edwin Lee <leeed2001@gmail.com>
2017-01-20 08:48:57 -0700

    Again more rule reworking

--- eptransition/versions/versions.py
+++ eptransition/versions/versions.py
@@ -1,20 +1,23 @@
 from eptransition.exceptions import ManagerProcessingException
 from eptransition.rules.base_rule import TransitionRule, OutputVariableTransitionRule
-from eptransition.rules.rules85to86 import (
-    branch as branch86,
-    airterminal_singleduct_inletsidemixer as inletmixer86,
-    airterminal_singleduct_supplysidemixer as supplymixer86,
-    otherequipment as otherequipment86,
-    zonehvac_airdistributionunit as zonehvac_adu86,
-    empd as empd86,
-    coilheatinggas as coilheatinggas86,
-    exteriorfuelequipment as exteriorequip86,
-    remove_dehumidification_fields as dehumid86,
-    chillerheater as chillerheater86,
-    setpoint_managers as setpointmanagers86,
-    ems_actuator as actuator86,
-    air_terminal_vav_reheat as vavreheat86,
-)
+from eptransition.rules.templates.remove_single_field import Rule as remove_field
+
+from eptransition.rules.rules85to86.branch import Rule as branch86
+from eptransition.rules.rules85to86.airterminal_singleduct_inletsidemixer import Rule as inletmixer86
+from eptransition.rules.rules85to86.airterminal_singleduct_supplysidemixer import Rule as supplymixer86
+from eptransition.rules.rules85to86.otherequipment import Rule as otherequipment86
+from eptransition.rules.rules85to86.zonehvac_airdistributionunit import Rule as zonehvac_adu86
+from eptransition.rules.rules85to86.empd import Rule as empd86
+from eptransition.rules.rules85to86.coilheatinggas import Rule as coilheatinggas86
+from eptransition.rules.rules85to86.exteriorfuelequipment import Rule as exteriorequip86
+from eptransition.rules.rules85to86.remove_dehumidification_fields import Rule as dehumid86a
+from eptransition.rules.rules85to86.remove_dehumidification_fields import Rule2 as dehumid86b
+from eptransition.rules.rules85to86.chillerheater import Rule as chillerheater86
+from eptransition.rules.rules85to86.setpoint_managers import Rule as setpointmanagers86a
+from eptransition.rules.rules85to86.setpoint_managers import Rule2 as setpointmanagers86b
+from eptransition.rules.rules85to86.ems_actuator import Rule as actuator86
+from eptransition.rules.rules85to86.air_terminal_vav_reheat import Rule as vavreheat86
+
 from eptransition.rules.rules86to87 import (
     controller_list as controller_list87,
     branch as branch87,
@@ -74,21 +77,21 @@ Transition84_85 = SingleTransition(8.4, 8.5,
                                    outputs=None)
 Transition85_86 = SingleTransition(8.5, 8.6,
                                    transitions=[
-                                       branch86.Rule(),
-                                       inletmixer86.Rule(),
-                                       supplymixer86.Rule(),
-                                       otherequipment86.Rule(),
-                                       zonehvac_adu86.Rule(),
-                                       empd86.Rule(),
-                                       coilheatinggas86.Rule(),
-                                       exteriorequip86.Rule(),
-                                       dehumid86.Rule(),
-                                       dehumid86.Rule2(),
-                                       chillerheater86.Rule(),
-                                       setpointmanagers86.Rule(),
-                                       setpointmanagers86.Rule2(),
-                                       actuator86.Rule(),
-                                       vavreheat86.Rule(),
+                                       branch86(),
+                                       inletmixer86(),
+                                       supplymixer86(),
+                                       otherequipment86(),
+                                       zonehvac_adu86(),
+                                       empd86(),
+                                       coilheatinggas86(),
+                                       exteriorequip86(),
+                                       dehumid86a(),
+                                       dehumid86b(),
+                                       chillerheater86(),
+                                       setpointmanagers86a(),
+                                       setpointmanagers86b(),
+                                       actuator86(),
+                                       vavreheat86(),
                                    ],
                                    outputs=None)
 Transition86_87 = SingleTransition(8.6, 8.7,
